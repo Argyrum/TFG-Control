f31da690259669bb8cea9c93cb6a8cc443bbd486
R S <Fact_Core@outlook.com>
2022-03-22 22:48:28 +0100

    Added Filter and Kd Tests | Cleaned up Tests folder

--- Matlab TFG/TFG_Preload.m
+++ Matlab TFG/TFG_Preload.m
@@ -1,6 +1,6 @@
 %Variables
 tau = 0.255;
-K = 50; % Resposta OP 2V 45 | OP 3V 50
+K = 61; % Resposta OP 2V 45 | OP 3V 50 | trueK 61
 trueK = 61; % rad/s / Vin -> 60,6~61,7
 Kpot = 1.688; % Vpot = -0.04006 + 1.688*angle (angle =/= roda, desplaçat tal que angle 0 a roda 247.5)
 Ktac = 0.01338; % Vtac = -0.04619 + 0.01338*w
